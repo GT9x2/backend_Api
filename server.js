4a046ff55aeebbe38bd6734771648daea1632389
BoatGT-9 <twz192545@gmail.com>
2023-11-12 19:39:10 +0700

    fix cors

--- server.js
+++ server.js
@@ -35,9 +35,23 @@ function initial(){
   })
 }
 
-app.use(cors());
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const whitelist = ['http://localhost:5000','https://extinct-ruby-cap.cyclic.app']
+const corsOption = {
+  origin:function(origin,callback){
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null,true)
+    } else {
+      callback(new Error('Not allow by CORS'))
+    }
+  }
+}
+  
 
 
 
@@ -51,6 +65,7 @@ app.listen(PORT, () => {
   console.log("เซอร์เวอร์ต่ออยู่ที่ http://localhost:" + PORT);
   
 });
+
 
 
 app.use("/books", LibraryRouter);
