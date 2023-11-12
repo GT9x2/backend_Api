4bdba2b8639a560d6ee273530da6527ad281a9bf
BoatGT-9 <twz192545@gmail.com>
2023-11-12 19:45:15 +0700

    cors

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ function initial(){
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors(corsOption));
 
 const whitelist = ['http://localhost:5000','https://extinct-ruby-cap.cyclic.app']
 const corsOption = {
