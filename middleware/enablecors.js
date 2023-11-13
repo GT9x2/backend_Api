d68c31e4b40cc5a5d1297f68c602538164d351b0
BoatGT-9 <twz192545@gmail.com>
2023-11-14 06:40:12 +0700

    fix enable cors

--- middleware/enablecors.js
+++ middleware/enablecors.js
@@ -1,6 +1,6 @@
 const enableCORS = (req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin","https://extinct-ruby-cap.cyclic.app");
+    "Access-Control-Allow-Origin","https://extinct-ruby-cap.cyclic.app/api/auth");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
