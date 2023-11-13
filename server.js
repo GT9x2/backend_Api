dc99cbc30942f48144fb27c6246fe41092088c60
BoatGT-9 <twz192545@gmail.com>
2023-11-13 17:41:07 +0700

    fix cors

--- server.js
+++ server.js
@@ -40,19 +40,21 @@ function initial(){
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const whitelist = ['http://localhost:5000','http://localhost:5173','https://extinct-ruby-cap.cyclic.app']
-const corsOption = {
-  origin:function(origin,callback){
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null,true)
-    } else {
-      callback(new Error('Not allow by CORS'))
-    }
-  }
-}
+// const whitelist = ['http://localhost:5000','http://localhost:5173','https://extinct-ruby-cap.cyclic.app']
+// const corsOption = {
+//   origin:function(origin,callback){
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null,true)
+//     } else {
+//       callback(new Error('Not allow by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOption));
-  
+// app.use(cors());
+app.use(cors({
+  origin: 'https://extinct-ruby-cap.cyclic.app'
+}));
 
 
 
