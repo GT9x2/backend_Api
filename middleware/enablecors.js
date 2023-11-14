0149b1dcd9612030c8bc738fc36c980d3efb41fb
BoatGT-9 <twz192545@gmail.com>
2023-11-14 07:26:37 +0700

    cors1

--- middleware/enablecors.js
+++ middleware/enablecors.js
@@ -1,6 +1,6 @@
 const enableCORS = (req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin","*");
+    "Access-Control-Allow-Origin","*","http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header('Access-Control-Allow-Credentials', true);
