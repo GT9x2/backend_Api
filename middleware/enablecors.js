0b142b959a3867b014a6c0bddd4b334ef5f18018
BoatGT-9 <twz192545@gmail.com>
2023-11-14 08:09:50 +0700

    resheader

--- middleware/enablecors.js
+++ middleware/enablecors.js
@@ -7,7 +7,7 @@ const enableCORS = (req, res, next) => {
   );
   res.header("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Max-Age", "1800")
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept ');
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 };
