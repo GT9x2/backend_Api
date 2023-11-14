6af45287fa38ad1a562784a7f021eb2b11c81472
BoatGT-9 <twz192545@gmail.com>
2023-11-14 07:43:36 +0700

    Age

--- middleware/enablecors.js
+++ middleware/enablecors.js
@@ -1,9 +1,14 @@
 const enableCORS = (req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin","*","http://localhost:5173","https://extinct-ruby-cap.cyclic.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    "Access-Control-Allow-Origin",
+    "*",
+    "http://localhost:5173",
+    "https://extinct-ruby-cap.cyclic.app"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Max-Age", "1800")
   res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 };
 
